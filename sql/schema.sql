a40b0cd7da0122e27d3bf0b0630c22349c494ae2
T2 <t2.tide@gmail.com>
2016-04-14 00:54:41 +0900

    tweaks

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE `posts` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `response` varchar(280) DEFAULT NULL,
     `created_at` datetime DEFAULT NULL,
-    `deleted_tat` datetime DEFAULT NULL,
+    `deleted_at` datetime,
     PRIMARY KEY (`id`)
     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
